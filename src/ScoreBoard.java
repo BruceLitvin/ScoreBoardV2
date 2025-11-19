d7f24acbd32df4b02491d6f47422091f31fc53a0
BruceLitvin <brucel15@nycstudents.net>
2025-11-18 19:07:16 -0500

    Skeleton

---
+++ src/ScoreBoard.java
@@ -0,0 +1,19 @@
+public class ScoreBoard {
+    private String word;
+    private String word2;
+
+    public ScoreBoard(String word1, String word2){
+        word=word1;
+        this.word2=word2;
+    }
+
+    public static int recordPlay(int num){
+        return 0;
+    }
+    public static int getScore(){
+        return 0;
+
+        }
+}
+
+
