5cfda5f48dcfa86fe7f34929277473a9803d9bff
BruceLitvin <brucel15@nycstudents.net>
2025-11-19 19:20:35 -0500

    V2

--- src/ScoreBoard.java
+++ src/ScoreBoard.java
@@ -1,19 +1,50 @@
 public class ScoreBoard {
-    private String word;
+    private  String word;
     private String word2;
+    private int scoreTeam1 =0;
+    private int scoreTeam2=0;
+    private boolean isActive=true;
+    String info;
 
-    public ScoreBoard(String word1, String word2){
-        word=word1;
-        this.word2=word2;
+    public ScoreBoard(String team1, String team2){
+        word=team1;
+        word2=team2;
+
     }
 
-    public static int recordPlay(int num){
-        return 0;
+    public void recordPlay(int num){
+
+        if(num==0){
+            isActive=!isActive;
+        }
+        if(isActive==true) {
+            scoreTeam1+=num;
+        }
+        if(isActive==false){
+            scoreTeam2+=num;
+        }
+        System.out.println(scoreTeam1);
+        System.out.println(scoreTeam2);
+
+
+
+
     }
-    public static int getScore(){
-        return 0;
+    public String getScore(){
+        String activeTeam=null;
+        if(isActive==true){
+            activeTeam=word;
+        }
+        if(isActive==false){
+            activeTeam=word2;
+        }
+         return (scoreTeam1+"-"+scoreTeam2+activeTeam);
 
         }
+
+
+
 }
+
 
 
